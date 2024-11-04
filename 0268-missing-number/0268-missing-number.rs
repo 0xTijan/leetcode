88a4affa6936c507ec7ac71890ba8c9d0a2d796d
Tijan <88556617+0xTijan@users.noreply.github.com>
2024-11-04 17:56:57 +0100

    Time: 0 ms (100%), Space: 2.4 MB (60.22%) - LeetHub

---
+++ 0268-missing-number/0268-missing-number.rs
@@ -0,0 +1,9 @@
+impl Solution {
+    pub fn missing_number(nums: Vec<i32>) -> i32 {
+        let n = nums.len() as i32;
+        let expected_sum = n * (n + 1) / 2;
+        let actual_sum: i32 = nums.iter().sum();
+
+        expected_sum - actual_sum
+    }
+}
